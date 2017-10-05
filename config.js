cb63c5bbbd31d55691442ada8f5fe25f2e07bceb
Keith POON <o.keithpoon@gmail.com>
2017-10-06 04:09:23 +0800

    simple web server
    add: web server enable response html, css, js, jpg, gif, png, woff, woff2
    add: default response index.html, default.html, home.html if request on a folder
    add: index.html

---
+++ config.js
@@ -0,0 +1,12 @@
+
+var port = 8000;
+var serverUrl = "127.0.0.1";
+
+var checkMimeType = true;
+
+var config = {
+	port: port,
+	serverUrl: serverUrl,
+	checkMimeType: checkMimeType
+};
+module.exports = config;
